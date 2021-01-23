14b6f6f6e36222e1490454807808596333a08593
Chris Baum <christopher.baum@valpo.edu>
2021-01-23 00:39:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,16 +2,14 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = [] #Class constant
+
   def initialize(title)
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
-  end
-
-  def genres
-
   end
 
 end
